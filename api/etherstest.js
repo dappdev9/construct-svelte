0321e0933a263d3509a0ff42c5c2a753af60dc8a
deepdappdev <62005234+deepdappdev@users.noreply.github.com>
2020-08-02 15:38:29 +0300

    up

--- api/etherstest.js
+++ api/etherstest.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     const { ethers } = require("ethers");
-    const INFURA_KEY = 'bf5f9a8e41a84dd18f0ad3824cd2dc67';
+    
     const network = "ropsten";
     const provider = ethers.getDefaultProvider(network, {
         // etherscan: YOUR_ETHERSCAN_API_KEY,
